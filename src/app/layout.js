3053bd69e076612bd21b9b1539b28dd14b07e86a
Dilkhush4707 <155545584+Dilkhush4707@users.noreply.github.com>
2026-03-07 11:38:12 +0530

    google search console

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
   metadataBase: new URL('https://drmayurkumargoyal.com'),
   title: {
     template: '%s',
-    default:'Mayur Childern Hospital | Pediatrician in Ajmer'
+    default: 'Mayur Childern Hospital | Pediatrician in Ajmer'
   },
   description: 'Best pediatrician in Ajmer, Rajasthan. Dr. Mayur Kumar Goyal specializes in child healthcare, newborn care, and vaccination services.',
   keywords: [
@@ -32,25 +32,26 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta name="google-site-verification" content="bniGqcb6lAS--Rg7LOlEfz6y46F-nTwr-R9q_PszKS4" />
         <Script
-         src="https://www.googletagmanager.com/gtag/js?id=G-TYC6WTHMGF"
-         strategy="afterInteractive"
-       />
-       
-       <Script id="google-analytics" strategy="afterInteractive">
-         {`
+          src="https://www.googletagmanager.com/gtag/js?id=G-TYC6WTHMGF"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
            window.dataLayer = window.dataLayer || [];
            function gtag(){dataLayer.push(arguments);}
            gtag('js', new Date());
            gtag('config', 'G-TYC6WTHMGF');
          `}
-       </Script>
+        </Script>
       </head>
       <body>
         <Navbar />
         <main>{children}</main>
         {/* <NewYearPopup/> */}
-        <FloatingContact/>
+        <FloatingContact />
         <Footer />
       </body>
     </html>
